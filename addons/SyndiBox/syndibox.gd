e2476fa14ca85cdc0f0fe709812da6a97bb9d054
Cullen <cullenjcarns@gmail.com>
2019-12-30 18:34:53 -0800

    Fixed crash and placement issue with cont text.

--- addons/SyndiBox/syndibox.gd
+++ addons/SyndiBox/syndibox.gd
@@ -818,12 +818,13 @@ func _physics_process(delta): # Called every step
 				# If there is no character in reference...
 				if !wr.get_ref():
 					# Carry on.
-					pass
+					continue
 				# If there is a character in reference...
 				else:
 					# Remove that character.
 					cur_char[i].free()
-					cur_tween[i].free()
+					if cur_tween.has(i):
+						cur_tween[i].free()
 			# Ready the dialog variables for the next string.
 			cur_speed = speed
 			cur_char = {}
@@ -832,6 +833,8 @@ func _physics_process(delta): # Called every step
 			str_line = 0
 			cur_set += 1
 			step = 0
+			heightTrack = 0
+			maxLineHeight = 0
 			step_pause = 0
 			escape = false
 			# Set our current string to the next string in the set.
